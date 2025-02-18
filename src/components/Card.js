08c21d694597ea81e15230982efc642d96fcee4d
itrohit45 <rohitwaghmare602@gmail.com>
2025-02-18 09:18:33 +0530

    first commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,12 +13,12 @@ function Card() {
       {
        allUser.map((value,index) => {
           return (
-            <div key={index} className="card m-1" style={{width:300}}>
+            <div key={index} className="card m-6" style={{width:300}}>
             <img src={value.avatar_url} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{value.login}</h5>
-              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <a href={value.url} className="btn btn-primary">Go somewhere</a>
+              <p className="card-text"></p>
+              <a href={value.url} className="btn btn-primary">About Us</a>
             </div>
           </div>
           
